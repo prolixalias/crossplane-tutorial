692f0f1e2eaed7b827d99c0caacf09afdcd897cf
Paul Talbot <prolixalias@gmail.com>
2024-09-19 07:31:28 -0400

    added ae and am

--- setup/00-bootstrap.sh
+++ setup/00-bootstrap.sh
@@ -38,28 +38,30 @@ echo "## Which Hyperscaler do you want to use?" | gum format
 
 HYPERSCALER=$(gum choose "aws" "azure" "local")
 
+clear
+
 rm -f .env
 
 echo "export HYPERSCALER=$HYPERSCALER" >> .env
 
-#########################
-# Control Plane Cluster #
-#########################
+###########################
+## Control Plane Cluster ##
+###########################
 
-KIND_EXPERIMENTAL_PROVIDER=nerdctl kind create cluster --config kind.yaml
+KIND_EXPERIMENTAL_PROVIDER=nerdctl kind create cluster --config kind.yaml || true
 
-# kubectl apply --filename https://raw.githubusercontent.com/kubernetes/ingress-nginx/main/deploy/static/provider/kind/deploy.yaml
-
-##############
-# Crossplane #
-##############
+################
+## Crossplane ##
+################
 
 helm repo add crossplane-stable https://charts.crossplane.io/stable
 helm repo update
 
 helm upgrade --install crossplane crossplane \
     --repo https://charts.crossplane.io/stable \
-    --namespace crossplane-system --create-namespace --wait
+    --namespace crossplane-system \
+    --create-namespace \
+    --wait
 
 kubectl apply --filename providers/provider-kubernetes-incluster.yaml
 
@@ -219,9 +221,19 @@ spec:
 
 fi
 
-kubectl create namespace cto-development 
-kubectl create namespace cto-integration 
-kubectl create namespace cto-production 
+kubectl create namespace cto-development || true
+kubectl create namespace cto-integration || true
+kubectl create namespace cto-production || true
+
+##############################
+## NGINX Ingress Controller ##
+##############################
+
+helm upgrade --install ingress-nginx ingress-nginx \
+  --repo https://kubernetes.github.io/ingress-nginx \
+  --namespace ingress-nginx \
+  --create-namespace \
+  --values bootstrap/nginx-ingress-controller/helm-values.yaml
 
 ##############
 ## Headlamp ##
@@ -232,25 +244,27 @@ kubectl -n kube-system create serviceaccount headlamp-admin
 helm upgrade --install headlamp headlamp \
    --repo https://headlamp-k8s.github.io/headlamp \
    --namespace kube-system \
-   --values bootstrap/headlamp/helm-values.yaml --wait
+   --values bootstrap/headlamp/helm-values.yaml \
+   --wait
 
 kubectl apply --filename bootstrap/headlamp/metrics-server-components.yaml
 kubectl create token headlamp --namespace kube-system
 
 ###########
-# Argo CD #
+# ArgoCD #
 ###########
 
 REPO_URL=$(git config --get remote.origin.url)
 # workaround to avoid setting up SSH key in ArgoCD
 REPO_URL=$(echo $REPO_URL | sed 's/git@github.com:/https:\/\/github.com\//') # replace git@github.com: to https://github.com/
 
-yq --inplace ".spec.source.repoURL = \"$REPO_URL\"" bootstrap/argocd/apps.yaml
+yq --inplace ".spec.source.repoURL = \"$REPO_URL\"" bootstrap/argocd/projects.yaml
 
 helm upgrade --install argocd argo-cd \
     --repo https://argoproj.github.io/argo-helm \
     --namespace argocd --create-namespace \
-    --values bootstrap/argocd/helm-values.yaml --wait
+    --values bootstrap/argocd/helm-values.yaml \
+    --wait
 
-kubectl apply --filename bootstrap/argocd/apps.yaml
+kubectl apply --filename bootstrap/argocd/projects.yaml
 
